41a775d3c95642c4bef6c42de1337dfd2e2631c1
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-08 17:30:51 +0100

    Changes

--- src/pages/Lobby/index.js
+++ src/pages/Lobby/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { Modal } from '@material-ui/core';
 import { useSelector, useDispatch } from "react-redux";
 
