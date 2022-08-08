6d5df06a20abe98b4d5c2e54b3abec6480bb7c64
Rameez <rameez0khawaja@gmail.com>
2022-08-08 10:46:29 +0100

    Folder set up

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Chat } from "./Chat";
+export { default as CreateGame } from "./CreateGame";
+export { default as JoinGame } from "./JoinGame";
+export { default as QuestionCard } from "./QuestionCard";
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Game } from "./Game";
+export { default as HighScores } from "./HighScores";
+export { default as Lobby } from "./Lobby";
+export { default as Results } from "./Results";
+export { default as WaitingRoom } from "./WaitingRoom";
