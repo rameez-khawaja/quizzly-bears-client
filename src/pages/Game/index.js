c0d8f62750d6321531c65feed463d65756113b38
eheath30 <elliot.heath@btinternet.com>
2022-08-09 11:48:49 +0100

    >

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Redirect } from 'react-router-dom';
-import { Modal } from '@material-ui/core';
+import React, { useState, useEffect, useRef } from "react";
+import { Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { QuestionCard } from "../../components";
 
 const Game = () => {
-    const state = useSelector(state => state.quizState.questionNumber);
-    console.log(state)
-    return <div>Game loads</div>
-}
+  const questionNumber = useSelector((state) => state.quizState.questionNumber);
+  const allQuestions = useSelector((state) => state.quizState.questions);
+  console.log(allQuestions)
+
+  return (
+    <section>
+      <div> hello, see below for le question carde </div>
+      <section>
+        {questionNumber <= 10 && (
+          <div>
+            <QuestionCard allQuestions={allQuestions[questionNumber-1]} />
+          </div>
+        )}
+      </section>
+      {questionNumber > 10 && <Navigate to="/results" />}
+    </section>
+  );
+};
 
 export default Game;
