4b8285176e45ceedb64833e49d52d34a2e3a2879
eheath30 <elliot.heath@btinternet.com>
2022-08-09 15:48:04 +0100

    updated landing page title

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import {motion} from "framer-motion"
 const Header = () => {
     return (
         <section className="container">
             <header className="text-center py-5 mx-5">
                 <div>
-                    <h1 className="text-light display-5">Quizzly Bears</h1>
+                    <motion.h1
+        className='text-light display-5'
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 0.7 }}
+        exit={{ opacity: 0, scale: 0, x: "-100vh"}}
+        whileHover={{ scale: 0.8 }}
+        transition={{
+            delay: 0.3,
+            x: { duration: 2 },
+            default: { ease: "linear" }
+          }}
+      >Quizzly Bears
+      </motion.h1>
                 </div>
                 <hr className="mx-5 bg-dark"/>
              </header>
