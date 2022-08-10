093a1325984f9bef435ba9255e072de61d191342
eheath30 <elliot.heath@btinternet.com>
2022-08-10 17:04:14 +0100

    updated classes!

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -93,7 +93,7 @@ export default function QuestionCard({ questionDetails, questionNumber }) {
     exit="exit"
     >
       <motion.div
-          class="questioncard row"
+          className="questioncard row"
           initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     exit={{ opacity: 0, scale: 0.5}}
